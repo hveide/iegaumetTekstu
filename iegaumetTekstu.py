9109ca2ed0931bc9d12f86817131e46efc2600b4
hveide <119410800+hveide@users.noreply.github.com>
2025-04-09 09:27:53 +0300

    macities(teksts) funkcija iesākta

--- iegaumetTekstu.py
+++ iegaumetTekstu.py
@@ -19,7 +19,37 @@ def ievade():
         teksts = teksts[:-1]
     macities(teksts)
 def macities(teksts):
-    print(teksts)
+    rindkopas = teksts.split('\n')
+    i = 0
+    for rindkopa in rindkopas:
+        if rindkopa == "":
+            rindkopas.pop(i)
+        i += 1
+    fragments = []
+    rindkopuIzvele = input("Izvēlaties rindkopas, kuras vēlaties mācīties, piemēram, 1, 1-3, vai p priekš pilna teksta: ")
+    if rindkopuIzvele.isdigit():
+        rindkopuIzvele = int(rindkopuIzvele) - 1
+        try:
+            fragments.append(rindkopas[rindkopuIzvele])
+        except IndexError:
+            print("Nepareizi ievadīts rindkopas numurs")
+            macities(teksts)
+    elif "-" in rindkopuIzvele:
+        rindkopa1, rindkopa2 = rindkopuIzvele.split('-')
+        try:
+            rindkopa1 = int(rindkopa1) - 1
+            rindkopa2 = int(rindkopa2) - 1
+        except ValueError:
+            print("Nepareiza rindkopu izvēle")
+            macities(teksts)
+        try:
+            rindkopas[rindkopa1] # Kļuda ja ievada a-b, tad 1
+            rindkopas[rindkopa2]
+            fragments = rindkopas[rindkopa1:rindkopa2]
+        except IndexError:
+            print("Rindkopu izvēle pāriet rindkopu skaitu")
+            macities(teksts)
+    print(fragments)
 
 if __name__ == "__main__":
     ievade()
