4ef59694ddd61ce28e93d25210cb35ee22bce961
hveide <119410800+hveide@users.noreply.github.com>
2025-04-29 17:00:42 +0300

    Pabeigts iegaumetTekstu()

--- iegaumetTekstu.py
+++ iegaumetTekstu.py
@@ -83,21 +83,82 @@ def pirmieBurti(fragments):
     parstradats = ' ' + ' '.join(burti)
     return parstradats
 
+
 def parbaudit(fragments):
-    ievade = """1. Loremtest ipsum dolor sit amet consectetur adipiscingtest elit. Quisque faucibus ex sapien vitae pellentesquetest sem placerat. In cursus mi pretium duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-2. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-3. Lorem ipsum dolor sit amet contestsectetur adiptestiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-4test. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.test In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-5. Lorem ipsum dolor sitetstt amet consectetur adipiscing elit. Qutestisque fauctestibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."""
-    ievade2 = """1. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-2. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-3. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-4. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-5. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."""
-    s = difflib.SequenceMatcher(None, fragments, ievade2)
-    precizitate = round(s.ratio(), 3)
-    print(f"Precizitāte ir {round(precizitate*100, 1)}%")
-    print(s.get_matching_blocks())
+    for i in range(20):
+        print("\n")
+    print("Ievadiet iegaumēto tekstu (Beigās Ctrl+Z + Enter): ")
+    lines = sys.stdin.readlines()
+    iegaumets = "".join(lines)
+
+    # Remove trailing newline often added by Ctrl+Z/D
+    if iegaumets.endswith('\n'):
+        iegaumets = iegaumets[:-1]
+
+    diffPrecizitate = difflib.SequenceMatcher(None, fragments, iegaumets)
+    precizitate = diffPrecizitate.ratio()
+    print(f"\nPrecizitāte: {precizitate*100:.1f}%")
+
+    kludas = 0
+    for tag, i1, i2, j1, j2 in diffPrecizitate.get_opcodes():
+        if tag != 'equal':
+            kludas += max(i2 - i1, j2 - j1)
+    print(f"Aptuvenais kļūdu skaits: {kludas}")
+
+    fragmentsRindas = fragments.splitlines()
+    iegaumetsRindas = iegaumets.splitlines()
+    diffRindas = difflib.SequenceMatcher(None, fragmentsRindas, iegaumetsRindas)
+    irKludas = False
+    labojums = []
+
+    for tag, i1, i2, j1, j2 in diffRindas.get_opcodes():
+        if tag == 'equal':
+            continue
+        irKludas = True
+        if labojums:
+            labojums.append("---")
+
+        if tag == 'replace':
+            if i1+1 == i2:
+                labojums.append(f"Kļūda {i2}. rindā:")
+            else:
+                labojums.append(f"Kļūdas no {i1+1}. līdz {i2}. rindai:")
+            n = max(i2 - i1, j2 - j1)
+            for k in range(n):
+                fragmentsIndex = i1 + k
+                iegaumetsIndex = j1 + k
+                fragmentsRinda = fragmentsRindas[fragmentsIndex] if fragmentsIndex < i2 else "Kļūda ievadē"
+                iegaumetsRinda = iegaumetsRindas[iegaumetsIndex] if iegaumetsIndex < j2 else "Kļūda ievadē"
+                labojums.append(f"+ {fragmentsRinda}")
+                labojums.append(f"- {iegaumetsRinda}")
+
+        elif tag == 'delete':
+            if i1+1 == i2:
+                labojums.append(f"Trūkst ievade {i2}. rindā:")
+            else:
+                labojums.append(f"Trūkst ievade no {i1+1}. līdz {i2}. rindai:")
+            for i in range(i1, i2):
+                labojums.append(f"+ {fragmentsRindas[i]}")
+
+        elif tag == 'insert':
+            if j1+1 == j2:
+                labojums.append(f"Lieka ievade {j2}. rindā:")
+            else:
+                labojums.append(f"Lieka ievade no {j1+1}. līdz {j2}. rindai:")
+            for j in range(j1, j2):
+                labojums.append(f"- {iegaumetsRindas[j]}")
+
+    if not irKludas:
+        print("Teksts iegaumēts bez kļūdām")
+    else:
+        print("\n".join(labojums).strip())
+    atkartot = input("Vai vēlaties vēlreiz mācīties? (J/N): ").upper()
+    if atkartot == "J":
+        turpinat = True
+        while turpinat:
+            turpinat = macities(teksts)
+    else:
+        exit()
 
 if __name__ == "__main__":
     ievade()
